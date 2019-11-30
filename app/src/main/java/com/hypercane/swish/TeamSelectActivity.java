c51a6ac95eaafdf62602a68ac72a47c59eb69a9c
AviusX <aviusanima@gmail.com>
2019-12-01 01:25:40 +0530

    Added functionality to trainingButton and newsButton.

---
+++ app/src/main/java/com/hypercane/swish/TeamSelectActivity.java
@@ -0,0 +1,14 @@
+package com.hypercane.swish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeamSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_select);
+    }
+}
